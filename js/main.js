179c67d7cc4c08ec0efac4d9b8244d4bef4681ae
Amal Joe R S <amaljoe88@gmail.com>
2023-04-02 10:19:31 +0530

    starter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const header = document.getElementById("header");
 window.onscroll = function () {
-    if (document.documentElement.scrollTop > 250) {
+    if (document.documentElement.scrollTop > window.innerHeight / 2) {
         header.className = "sticky";
     } else {
         header.className = "";
     }
 };
+
+function onClickMenu(x) {
+    document.getElementById("header").classList.toggle("change");
+}
