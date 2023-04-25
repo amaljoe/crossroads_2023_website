ff3f86d3440a9d1e0d3e13bbdd3029bb734d192f
amaljoe <amaljoe88@gmail.com>
2023-04-25 11:20:05 +0530

    fix side nav scroll and reverse transition

--- js/main.js
+++ js/main.js
@@ -1,33 +1,40 @@
 const header = document.getElementById("header");
 const proshows = document.getElementById("proshows");
+const body = document.getElementById("body");
 const proshowsHeading = document.getElementById("proshows-heading");
 var showNav = false;
 
 window.onscroll = function () {
-    const proshowsHeadingTop = proshowsHeading.getBoundingClientRect().top;
-    // proshow heading animation
-    if (document.documentElement.scrollTop > window.innerHeight / 8 || (proshowsHeadingTop < window.innerHeight && document.documentElement.scrollTop > 0)) {
-        proshowsHeading.classList.add("scrolled");
-    } else {
-        proshowsHeading.classList.remove("scrolled");
-    }
+  const proshowsHeadingTop = proshowsHeading.getBoundingClientRect().top;
+  // proshow heading animation
+  if (
+    document.documentElement.scrollTop > window.innerHeight / 8 ||
+    (proshowsHeadingTop < window.innerHeight &&
+      document.documentElement.scrollTop > 0)
+  ) {
+    proshowsHeading.classList.add("scrolled");
+  } else {
+    proshowsHeading.classList.remove("scrolled");
+  }
 
-    // proshows animation and sticky header
-    if (document.documentElement.scrollTop > window.innerHeight / 3 * 2) {
-        proshows.classList.remove("start");
-        header.classList.add("sticky");
-    } else {
-        proshows.classList.add("start");
-        header.classList.remove("sticky");
-    }
+  // proshows animation and sticky header
+  if (document.documentElement.scrollTop > (window.innerHeight / 3) * 2) {
+    proshows.classList.remove("start");
+    header.classList.add("sticky");
+  } else {
+    proshows.classList.add("start");
+    header.classList.remove("sticky");
+  }
 };
 
 function onClickMenu(x) {
-    if (showNav) {
-        header.classList.remove("change");
-        showNav = false;
-    } else {
-        header.classList.add("change");
-        showNav = true;
-    }
+  if (showNav) {
+    header.classList.remove("change");
+    body.classList.remove("no-scroll");
+    showNav = false;
+  } else {
+    header.classList.add("change");
+    body.classList.add("no-scroll");
+    showNav = true;
+  }
 }
